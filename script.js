ae8eff0eaf42c9080bcdc43af7893912ad523abb
Paavana-2006 <paavanapranavasriyalla@gmail.com>
2026-06-05 12:56:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Navbar Scroll Effect
+
+window.addEventListener("scroll", function(){
+
+    let navbar = document.getElementById("navbar");
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("scrolled");
+
+    }
+    else{
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
+
+
+// Hamburger Menu
+
+let menuToggle = document.getElementById("menu-toggle");
+
+let navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", function(){
+
+    navLinks.classList.toggle("active");
+
+});
